5cefa4a963325a485e2b2e4da33841f4fab36d0a
James Goode <james_goode@hotmail.co.uk>
2021-05-14 14:44:36 +0100

    Add header links and some styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-    <router-view />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+      <div class="header-bar">
+        <router-link to="/" class="header-link home-link">The Unfinished Tome</router-link>
+        <div class="header-links">
+          <router-link to="/events" class="header-link header-page-link">Events</router-link>
+        </div>
+      </div>
+    </header>
+    <main>
+      <router-view />
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </main>
   </div>
 </template>
 
@@ -18,6 +28,10 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +39,52 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+main {
+  text-align: center;
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+header {
+  height: auto;
+  background-color: #222;
+}
+
+.header-bar {
+  padding: 10px 0;
+  margin: auto 50px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.header-links {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.header-link {
+  color: #9d9d9d !important;
+  text-decoration: none !important;
+  margin: auto 0px;
+  white-space: nowrap;
+}
+
+.header-link:hover {
+  color: lightgrey !important;
+}
+
+.home-link {
+  position: relative;
+  font-size: 30px;
+  line-height: 35px;
+  margin-right: 30px;
+}
+
+.header-page-link {
+  line-height: 35px;
+  margin-right: 20px;
+  font-weight: bold;
 }
 </style>
