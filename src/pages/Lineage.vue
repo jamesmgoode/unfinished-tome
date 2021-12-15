45605b6828a95878015a9cba7b0c6d0afcda7e45
James Goode <james_goode@hotmail.co.uk>
2021-12-15 18:12:52 +0000

    Add lineage page

---
+++ src/pages/Lineage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lineage">
+    <h1>Lineage</h1>
+    <h2>Beastkin</h2>
+    <p>
+      Beastkin are often instinctive; they make snap judgements and then swiftly
+      pursue them rather than slowing down and planning ahead. While this can
+      certainly be a weakness there are many jobs where it is a distinct
+      advantage. Many beastkin take work as guides, bodyguards and trackers,
+      where instinctive action can be make or break for survival. It would be a
+      mistake to think that beastkin are simple and they can find success in
+      many walks of life.
+    </p>
+    <p>
+      Beastkin are often imbued with characteristics of their animal heritage
+      (owls are wise, wolves stalk their prey etc) and retain a link back to
+      their inner animal.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lineage",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
